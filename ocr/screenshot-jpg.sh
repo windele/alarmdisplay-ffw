47bce734ce2301edf01e13179f3f128f6cddbcee
Andreas Fendt <andreasfendt@gmx.net>
2015-09-06 22:39:34 +0200

    - Das Skript faxrcvd hinzugefügt - Es wird nicht mehr /tmp als Temporäres Verzeichnis verwendet sondern     das Verzeichnis /tmp/alarmfax - Die Datei readfile.php überarbeitet:     - Das Programm lp wird statt lpr verwendet     - In der E-Mail wird das Alarmfax mit Angehängt     - Die erkennung findet nun mit Regulären Ausdrücken statt     - Verschiedene kleinere Korrekturen

--- ocr/screenshot-jpg.sh
+++ ocr/screenshot-jpg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export DISPLAY=:0
-wkhtmltoimage --height 1280 --width 1920 --javascript-delay 12500 --quality 100 http://localhost/alarmdisplay /tmp/screenshot.jpg
-convert /tmp/screenshot.jpg -resize 50% /tmp/screenmail.jpg
-chmod 755 /tmp/screen*
+wkhtmltoimage --height 1280 --width 1920 --javascript-delay 12500 --quality 100 http://localhost/alarmdisplay /tmp/alarm/screenshot.jpg
+convert /tmp/alarm/screenshot.jpg -resize 50% /tmp/alarm/screenmail.jpg
+chmod 644 /tmp/screen*
